d9d360c552465bc815bd928c26783c64892a23e7
Natasza ASzatan <natiye.nb@gmai.com>
2023-11-14 14:42:35 +0000

    started the assigment

---
+++ ex13.js
@@ -0,0 +1,26 @@
+const redApple = {
+    color: 'red',
+    weightInGrams: 150
+}
+
+const propertyName = findObjectProperty(redApple, function(propertyValue) {
+    return propertyValue === 'red';
+})
+console.log(propertyName); // color
+
+const john = {
+    name: 'John',
+    bestFriend: {
+        name: 'Adam'
+    }
+}
+
+const adamPropertyName = findObjectProperty(john, function(propertyValue) {
+    return propertyValue && propertyValue.name === 'Adam';
+})
+console.log(adamPropertyName); // bestFriend
+
+
+function findObjectProperty(object, func){
+    
+}
