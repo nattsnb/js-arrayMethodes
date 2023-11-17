7ca0fb76b507fd6931b9219400e4af4b88643bcc
Natasza ASzatan <natiye.nb@gmai.com>
2023-11-17 13:56:42 +0000

    fixed function to return function

--- ex14.js
+++ ex14.js
@@ -1,22 +1,23 @@
 const getRandomDigit = getRandomCharacterGenerator('0123456789');
 
-console.log(getRandomDigit); // returns a random string that contains a single digit
+console.log(getRandomDigit()); // returns a random string that contains a single digit
 
 const getRandomABC = getRandomCharacterGenerator('abcABC');
-console.log(getRandomABC); // returns a random string that is a, A, b, B, c, or C
+console.log(getRandomABC()); // returns a random string that is a, A, b, B, c, or C
 
 
 function getRandomCharacterGenerator(inputString) {
-    const inputArray = inputString.split("")
-    const inputLength = inputArray.length
-    const randomCharIndex = getRandomIntegerGenerator(inputLength)
-    return inputArray[randomCharIndex]
-
+    return function() {
+        const inputArray = inputString.split("")
+        const inputLength = inputArray.length
+        const randomCharIndex = getRandomIntegerGenerator(inputLength)
+        return inputArray[randomCharIndex]
+    }
 }
 
 
 function getRandomIntegerGenerator(max){
-    min = 0;
+    const min = 0;
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min))
 }
